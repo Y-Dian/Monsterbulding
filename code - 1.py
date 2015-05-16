42704ad9a4861f442a1f8d4945fe05cb78433b44
Zohar <zdianling@gmail.com>
2015-05-16 16:47:47 +0800

    add codes

---
+++ code - 1.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# 怪物大楼（欢迎语）代码示例。
+print("Welcome MonsterBuilding")
+print("You only have 100 bullets,you have to use the bullets to kill monsters.")
---
+++ code - 2.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# 怪物大楼（子弹数与怪物数）代码示例。
+print("Welcome MonsterBuilding")
+print("You only have 100 bullets,you have to use the bullets to kill monsters.")
+bullet = 100
